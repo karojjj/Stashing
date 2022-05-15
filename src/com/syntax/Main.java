1abc35bdf1300c187359e647b471038c0f82d130
Medine Kaya <99620226+karojjj@users.noreply.github.com>
2022-05-15 12:48:56 -0400

    the settings of the car set 95mhz

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -10,5 +10,10 @@ public class Main {
         basicFutures.headlights="Blue";
         basicFutures.registrationNumbers="shsosm";
 
+        // Lets write our own code
+        System.out.println("Welcome to my Car");
+        System.out.println("The radio is set to 80MHZ");
+        System.out.println("Turn on the air conditioner");
+        
     }
 }
