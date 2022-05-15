58fa2c84249e16e27482793b1eea477cf993c76c
Medine Kaya <99620226+karojjj@users.noreply.github.com>
2022-05-15 12:05:22 -0400

    the red toyota car is ready with some basic features

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         BasicFutures basicFutures = new BasicFutures();
-
+        basicFutures.color="Red";
+        basicFutures.doors="4";
+        basicFutures.engineCapacity="V8";
+        basicFutures.headlights="Blue";
+        basicFutures.registrationNumbers="shsosm";
 
     }
 }
