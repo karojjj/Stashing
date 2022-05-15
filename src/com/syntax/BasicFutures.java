3fa3c62401b55d7229bddc39f36f72c33589a2df
Medine Kaya <99620226+karojjj@users.noreply.github.com>
2022-05-15 11:01:38 -0400

    the basic structure of the car is ready, working on advance features

---
+++ src/com/syntax/BasicFutures.java
@@ -0,0 +1,10 @@
+package com.syntax;
+
+public class BasicFutures {
+    String engineCapacity;
+    String color;
+    String doors;
+    String headlights;
+    String registrationNumbers;
+
+}
---
+++ src/com/syntax/Main.java
@@ -0,0 +1,10 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BasicFutures basicFutures = new BasicFutures();
+
+
+    }
+}
